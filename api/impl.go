6be152db4a8fb655fdaa000be42bf1749841c986
Marvin Fuchs <info@marvin-fuchs.de>
2025-10-23 16:31:26 +0200

    feat: Add health endpoint (#28)
    * Add health endpoint
    
    * Add tag
    
    * fmt
    
    * Add tests

---
+++ api/impl.go
@@ -0,0 +1,21 @@
+package api
+
+import "context"
+
+// ensure that we've conformed to the `ServerInterface` with
+// a compile-time check
+var _ StrictServerInterface = (*Server)(nil)
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+// GetHealth implements StrictServerInterface.
+func (s *Server) GetHealth(
+	ctx context.Context,
+	request GetHealthRequestObject,
+) (GetHealthResponseObject, error) {
+	return GetHealth200Response{}, nil
+}
